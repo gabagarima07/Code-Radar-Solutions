8730655400050f88cd93503e4d251caa2c2ec627
gabagarima07 <gabagarima07@gmail.com>
2025-03-29 11:56:11 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -1,26 +1,13 @@
 // Your code here...
-#include<stdio.h>
-int factorial(int num)
-{
-    if (num == 1)
-    {
-        return 1;
-    }
-    else{
-        return(num*factorial(num - 1));
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
 
-    }
+# Example usage
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is {factorial(num)}")
 
-}
-int main()
-{
-    int num;
-    printf("enter the number you want factorial of:\n");
-    scanf("%d",&num);
-    printf("the factorial of %d is %d",num,factorial(num));
-    return 0;
-
-}
 
     
 
