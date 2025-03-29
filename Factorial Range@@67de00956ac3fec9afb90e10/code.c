72966f7fbd0c4d228a4f25c6e226050940dcf156
gabagarima07 <gabagarima07@gmail.com>
2025-03-29 12:00:59 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -1,13 +1,12 @@
 // Your code here...
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n - 1)
-
-# Example usage
-num = int(input("Enter a number: "))
-print(f"Factorial of {num} is {factorial(num)}")
-
+void factorialRange(int n){
+    if (n == 0 or n == 1){
+        return 1;
+    }
+    else{
+        return n*(factorialRange(n-1));
+    }
+}
 
     
 
